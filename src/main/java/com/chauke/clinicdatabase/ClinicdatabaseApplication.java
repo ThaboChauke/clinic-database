9ff5a4510897ae54f55dd1f314ab255f9f2e1d31
ThaboChauke <chaukethabo16@gmail.com>
2025-02-12 22:32:21 +0200

    project init

---
+++ src/main/java/com/chauke/clinicdatabase/ClinicdatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.chauke.clinicdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClinicdatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClinicdatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/chauke/clinicdatabase/ClinicdatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.chauke.clinicdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClinicdatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
